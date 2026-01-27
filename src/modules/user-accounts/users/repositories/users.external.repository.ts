6b82563a6e21c4c3b89dd77757eef9f7005e633b
VladSapozhnik <vladbars2@gmail.com>
2026-01-27 16:36:22 +0200

    add external user rep

--- src/modules/user-accounts/users/repositories/users.external.repository.ts
+++ src/modules/user-accounts/users/repositories/users.external.repository.ts
@@ -2,72 +2,80 @@ import { HttpStatus, Injectable } from '@nestjs/common';
 import { DomainException } from '../../../../core/exceptions/domain-exceptions';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UsersExternalRepository {
   constructor(@InjectDataSource() private readonly dataSource: DataSource) {}
 
   async getUserByLoginOrEmail(login: string, email: string) {
-    // return  await this.dataSource.query(``);
-    // return this.UserModel.findOne({ $or: [{ login }, { email }] });
+    const [existUser]: User[] = await this.dataSource.query(
+      'SELECT * FROM public.users WHERE login = $1 OR email = $2',
+      [login, email],
+    );
+
+    return existUser;
   }
 
   async findUserByCode(code: string) {
-    // return this.UserModel.findOne({
-    //   'emailConfirmation.confirmationCode': code,
-    // });
+    const [user]: User[] = await this.dataSource.query(
+      `SELECT * FROM users WHERE "confirmationCode" = $1;`,
+      [code],
+    );
+
+    return user;
   }
 
   async getUserById(id: string) {
-    // return this.UserModel.findOne({
-    //   _id: new Types.ObjectId(id),
-    // });
+    const [user]: User[] = await this.dataSource.query(
+      `SELECT * FROM users WHERE id = $1`,
+      [id],
+    );
+
+    return user;
   }
 
   async findUserByEmail(email: string) {
-    // const user: UserDocument | null = await this.UserModel.findOne({
-    //   email,
-    // });
-    //
-    // if (!user) {
-    //   throw new DomainException({
-    //     status: HttpStatus.BAD_REQUEST,
-    //     errorsMessages: [
-    //       {
-    //         message: 'Input incorrect email',
-    //         field: 'email',
-    //       },
-    //     ],
-    //   });
-    // }
-    //
-    // return user;
+    const [existUser]: User[] = await this.dataSource.query(
+      `SELECT * FROM users WHERE email = $1`,
+      [email],
+    );
+
+    if (!existUser) {
+      throw new DomainException({
+        status: HttpStatus.BAD_REQUEST,
+        errorsMessages: [
+          {
+            message: 'Input incorrect email',
+            field: 'email',
+          },
+        ],
+      });
+    }
+
+    return existUser;
   }
 
   async findByLoginOrEmail(loginOrEmail: string) {
-    // const existUser: UserDocument | null = await this.UserModel.findOne({
-    //   $or: [{ login: loginOrEmail }, { email: loginOrEmail }],
-    // });
-    //
-    // if (!existUser) {
-    //   throw new DomainException({
-    //     status: HttpStatus.UNAUTHORIZED,
-    //     errorsMessages: [
-    //       {
-    //         message: 'Invalid login or password',
-    //         field: 'loginOrEmail',
-    //       },
-    //     ],
-    //   });
-    // }
-    //
-    // return existUser;
-  }
+    const [existUser]: User[] = await this.dataSource.query(
+      `SELECT * FROM users WHERE login = $1 OR email = $1;`,
+      [loginOrEmail],
+    );
 
-  // async save(user: UserDocument): Promise<string> {
-  //   await user.save();
-  //   return user._id.toString();
-  // }
+    if (!existUser) {
+      throw new DomainException({
+        status: HttpStatus.UNAUTHORIZED,
+        errorsMessages: [
+          {
+            message: 'Invalid login or password',
+            field: 'loginOrEmail',
+          },
+        ],
+      });
+    }
+
+    return existUser;
+  }
 
   async removeUser(id: string) {
     await this.dataSource.query(`DELETE FROM public.users WHERE id = $1;`, [
