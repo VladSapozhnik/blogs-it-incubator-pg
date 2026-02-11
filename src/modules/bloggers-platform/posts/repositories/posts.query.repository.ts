af34059316e884f2c54a5546d2b9ac920a5a2fe3
VladSapozhnik <vladbars2@gmail.com>
2026-02-11 13:20:49 +0200

    fix

--- src/modules/bloggers-platform/posts/repositories/posts.query.repository.ts
+++ src/modules/bloggers-platform/posts/repositories/posts.query.repository.ts
@@ -1,4 +1,3 @@
-import { Post } from '../entities/post.entity';
 import { GetPostsQueryParamsDto } from '../dto/post-query-input.dto';
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { DomainException } from '../../../../core/exceptions/domain-exceptions';
@@ -52,27 +51,6 @@ export class PostsQueryRepository {
       posts,
       totalCount,
     };
-  }
-
-  async getPostById(id: string): Promise<Post> {
-    const [existPost]: Post[] = await this.dataSource.query(
-      `SELECT * FROM posts WHERE id = $1`,
-      [id],
-    );
-
-    if (!existPost) {
-      throw new DomainException({
-        status: HttpStatus.NOT_FOUND,
-        errorsMessages: [
-          {
-            message: 'Post not found',
-            field: 'post',
-          },
-        ],
-      });
-    }
-
-    return existPost;
   }
 
   async getPostByIdWithStatus(
