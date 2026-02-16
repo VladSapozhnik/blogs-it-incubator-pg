48466af7239ac1b03870becacdf88817990f27e4
VladSapozhnik <vladbars2@gmail.com>
2026-02-16 13:19:40 +0200

    fix testing 2

--- src/modules/testing/testing.controller.ts
+++ src/modules/testing/testing.controller.ts
@@ -10,18 +10,28 @@ export class TestingController {
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteAll(): Promise<void> {
     // Получаем все таблицы в схеме public
-    const tables: { table_name: string }[] = await this.dataSource.query(
-      `SELECT table_name FROM information_schema.tables
-             WHERE table_schema='public' AND table_type='BASE TABLE';`,
-    );
+    // const tables: { table_name: string }[] = await this.dataSource.query(
+    //   `SELECT table_name FROM information_schema.tables
+    //          WHERE table_schema='public' AND table_type='BASE TABLE';`,
+    // );
+    //
+    // // Чистим все таблицы параллельно
+    // if (tables.length > 0) {
+    //   for (const table of tables) {
+    //     await this.dataSource.query(
+    //       `DELETE FROM "${table.table_name}" CASCADE;`,
+    //     );
+    //   }
+    // }
+    const tables: { table_name: string }[] = await this.dataSource.query(`
+      SELECT table_name
+      FROM information_schema.tables
+      WHERE table_schema = 'public'
+        AND table_type = 'BASE TABLE';
+    `);
 
-    // Чистим все таблицы параллельно
-    if (tables.length > 0) {
-      for (const table of tables) {
-        await this.dataSource.query(
-          `DELETE FROM "${table.table_name}" CASCADE;`,
-        );
-      }
+    for (const { table_name } of tables) {
+      await this.dataSource.query(`DELETE FROM "${table_name}";`);
     }
   }
 }
