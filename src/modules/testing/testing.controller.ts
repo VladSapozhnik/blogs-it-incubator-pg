3b68aa89efbf85907b7515684d7b90567b1a381c
VladSapozhnik <vladbars2@gmail.com>
2026-02-16 13:33:52 +0200

    fix testing 4

--- src/modules/testing/testing.controller.ts
+++ src/modules/testing/testing.controller.ts
@@ -10,22 +10,23 @@ export class TestingController {
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteAll(): Promise<void> {
     try {
-      // Получаем список всех таблиц, если их много,
-      // либо перечисляем вручную:
-      const entities = this.dataSource.entityMetadatas;
-      const tableNames = entities
-        .map((entity) => `"${entity.tableName}"`)
-        .join(', ');
-
-      // Выполняем очистку
-      if (tableNames.length > 0) {
-        await this.dataSource.query(
-          `TRUNCATE ${tableNames} RESTART IDENTITY CASCADE;`,
-        );
-      }
+      await this.dataSource.query(`
+      TRUNCATE TABLE 
+        "users_security_devices",
+        "security_devices",
+        "password_recoveries",
+        "comment_likes",
+        "post_likes",
+        "comments",
+        "posts",
+        "blogs",
+        "users"
+      CASCADE;
+    `);
     } catch (e) {
-      console.error('Ошибка при очистке БД:', e);
-      throw e; // Это пробросит ошибку выше и поможет увидеть её в логах
+      // Если здесь будет 500, в консоли бэкенда будет написано, какой таблицы не хватает
+      console.error('ОШИБКА ОЧИСТКИ БАЗЫ:', e.message);
+      throw e;
     }
   }
 }
