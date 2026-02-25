7fb1961a59ecb588963514222ba3bccfa9baadfa
VladSapozhnik <vladbars2@gmail.com>
2026-02-25 13:00:11 +0200

    migration testing to typeorm

--- src/modules/testing/testing.controller.ts
+++ src/modules/testing/testing.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import { SecurityDevice } from '../user-accounts/security-devices/entities/security-device.entity';
+import { PasswordRecovery } from '../user-accounts/password-recovery/entities/password-recovery.entity';
+import { User } from '../user-accounts/users/entities/user.entity';
 
 @Controller('testing')
 export class TestingController {
@@ -10,19 +13,20 @@ export class TestingController {
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteAll(): Promise<void> {
     try {
-      await this.dataSource.query(`
-      TRUNCATE TABLE 
-        "users_security_devices",
-        "security_devices",
-        "password_recoveries",
-        "comment_likes",
-        "post_likes",
-        "comments",
-        "posts",
-        "blogs",
-        "users"
-      CASCADE;
-    `);
+      const entities = [
+        SecurityDevice,
+        PasswordRecovery,
+        // CommentLike,
+        // PostLike,
+        // Comment,
+        // Post,
+        // Blog,
+        User,
+      ];
+
+      for (const entity of entities) {
+        await this.dataSource.getRepository(entity).clear();
+      }
     } catch (e) {
       // Если здесь будет 500, в консоли бэкенда будет написано, какой таблицы не хватает
       console.error('ОШИБКА ОЧИСТКИ БАЗЫ:', e.message);
