5388c7947dfc6bc9df1ecb8e73c1b55105ae9f23
VladSapozhnik <vladbars2@gmail.com>
2026-03-11 18:53:50 +0200

    fix1

--- src/modules/bloggers-platform/posts/repositories/posts.query.external.repository.ts
+++ src/modules/bloggers-platform/posts/repositories/posts.query.external.repository.ts
@@ -70,7 +70,7 @@ export class PostsQueryExternalRepository {
       .addSelect('b.name', 'blogName')
       .where('p."blogId" = :blogId', { blogId });
 
-    const totalCount: number = await query.clone().getCount();
+    const totalCount: number = await query.getCount();
 
     const posts: PostWithStatusRowType[] = await query
       .orderBy(sortByMapPosts[queryDto.sortBy], queryDto.sortDirection)
