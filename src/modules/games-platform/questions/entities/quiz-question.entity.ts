bd068fa16917badf393a4ab136597f5c08c66e26
VladSapozhnik <vladbars2@gmail.com>
2026-03-27 11:06:25 +0200

    fix questionIds

--- src/modules/games-platform/questions/entities/quiz-question.entity.ts
+++ src/modules/games-platform/questions/entities/quiz-question.entity.ts
@@ -16,7 +16,7 @@ export class QuizQuestion {
   id: string;
   @Column({ type: 'varchar' })
   body: string;
-  @Column({ type: 'varchar' })
+  @Column({ array: true, type: 'text' })
   correctAnswers: string[];
   @Column({ type: 'boolean', default: false })
   published: boolean;
