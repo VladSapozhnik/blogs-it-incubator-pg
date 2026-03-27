c87900aae08db2b5a67e87bead80c712b1f5076c
VladSapozhnik <vladbars2@gmail.com>
2026-03-27 19:30:13 +0200

    fix finish game

--- src/modules/games-platform/pair-games/application/pair-games.service.ts
+++ src/modules/games-platform/pair-games/application/pair-games.service.ts
@@ -59,48 +59,4 @@ export class PairGamesService {
     // Сохраняем статус в базу
     await this.pairGameRepository.savePairGame(game);
   }
-
-  // async finishGameAndAssignBonus(game: PairGame) {
-  //   const lastAnswerP1: PlayerAnswer | null =
-  //     await this.playerAnswerRepository.getLastAnswer(
-  //       game.id,
-  //       game.firstPlayerId,
-  //     );
-  //   const lastAnswerP2: PlayerAnswer | null =
-  //     await this.playerAnswerRepository.getLastAnswer(
-  //       game.id,
-  //       game.secondPlayerId!,
-  //     );
-  //
-  //   if (!lastAnswerP1 || !lastAnswerP2) return;
-  //
-  //   const p1IsFaster: boolean = lastAnswerP1.addedAt < lastAnswerP2.addedAt;
-  //
-  //   const fastPlayerId: string = p1IsFaster
-  //     ? game.firstPlayerId
-  //     : game.secondPlayerId!;
-  //
-  //   const hasCorrectAnswers: boolean =
-  //     await this.playerAnswerRepository.hasCorrectAnswers(
-  //       game.id,
-  //       fastPlayerId,
-  //     );
-  //
-  //   if (hasCorrectAnswers) {
-  //     const playerProgress: PlayerProgress | null =
-  //       await this.playerProgressRepository.getPlayerProgress(
-  //         game.id,
-  //         fastPlayerId,
-  //       );
-  //
-  //     if (playerProgress) {
-  //       playerProgress.incrementScore();
-  //       await this.playerProgressRepository.savePlayerProgress(playerProgress);
-  //     }
-  //   }
-  //
-  //   game.finishGame();
-  //
-  //   await this.pairGameRepository.savePairGame(game);
-  // }
 }
