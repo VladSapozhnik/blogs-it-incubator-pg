a29304345b25a06e58bada6575cec3c0bc699d1c
VladSapozhnik <vladbars2@gmail.com>
2026-03-27 11:47:52 +0200

    add game question

---
+++ src/modules/games-platform/games-platform.module.ts
@@ -0,0 +1,82 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateQuestionUseCase } from './questions/application/usecases/create-question.usecase';
+import { UpdateQuestionUseCase } from './questions/application/usecases/update-question.usecase';
+import { UpdatePublishUseCase } from './questions/application/usecases/update-publish.usecase';
+import { RemoveQuestionUseCase } from './questions/application/usecases/remove-question.usecase';
+import { GetQuestionByIdQueryHandler } from './questions/application/queries/get-question-by-id.query';
+import { GelAllQuestionsQueryHandler } from './questions/application/queries/gel-all-questions.query';
+import { QuizQuestion } from './questions/entities/quiz-question.entity';
+import { QuizQuestionsSaController } from './questions/quiz-questions-sa.controller';
+import { QuizQuestionsService } from './questions/application/quiz-questions.service';
+import { QuizQuestionRepository } from './questions/repositories/quiz-question,repository';
+import { QuizQuestionQueryRepository } from './questions/repositories/quiz-question.query.repository';
+// import { PairGamesController } from './pair-games/pair-games.controller';
+// import { PairGamesService } from './pair-games/application/pair-games.service';
+// import { PairGamesRepository } from './pair-games/repositories/pair-games.repository';
+// import { PairGamesQueryRepository } from './pair-games/repositories/pair-games.query.repository';
+// import { ConnectCurrentUserUseCase } from './pair-games/application/usecases/connect-current-user.usecase';
+// import { SendNextAnswerUseCase } from './pair-games/application/usecases/send-next-answer.usecase';
+// import { GetGameByIdQueryHandler } from './pair-games/application/queries/get-game-by-id.query';
+// import { GetMyCurrentPairGameQueryHandler } from './pair-games/application/queries/get-my-current-pair-game.query';
+// import { PairGame } from './pair-games/entities/pair-game.entity';
+// import { PlayerAnswer } from './pair-games/entities/player-answer.entity';
+import { QuizQuestionQueryExternalRepository } from './questions/repositories/quiz-question.query.external,repository';
+// import { PlayerProgress } from './pair-games/entities/player-progress.entity';
+// import { PlayerAnswerService } from './pair-games/application/player-answer.service';
+// import { PlayerAnswerRepository } from './pair-games/repositories/player-answer.repository';
+// import { PlayerProgressRepository } from './pair-games/repositories/player-progress.repository';
+// import { GetPlayerAnswerByIdQueryHandler } from './pair-games/application/queries/get-player-answer-by-id.query';
+// import { PlayerAnswerQueryRepository } from './pair-games/repositories/player-answer.query.repository';
+// import { PlayerProgressQueryRepository } from './pair-games/repositories/player-progress.query.repository';
+// import { PairGamesQueryService } from './pair-games/application/pair-games.query.service';
+import { UserAccountsModule } from '../user-accounts/user-accounts.module';
+// import { GetGameQueryHandler } from './pair-games/application/queries/get-game.query';
+
+const useCases = [
+  CreateQuestionUseCase,
+  UpdateQuestionUseCase,
+  UpdatePublishUseCase,
+  RemoveQuestionUseCase,
+  GetQuestionByIdQueryHandler,
+  GelAllQuestionsQueryHandler,
+  // ConnectCurrentUserUseCase,
+  // SendNextAnswerUseCase,
+  // GetGameByIdQueryHandler,
+  // GetMyCurrentPairGameQueryHandler,
+  // GetPlayerAnswerByIdQueryHandler,
+  // GetGameQueryHandler,
+];
+
+@Module({
+  imports: [
+    UserAccountsModule,
+    TypeOrmModule.forFeature([
+      QuizQuestion,
+      // PairGame,
+      // PlayerAnswer,
+      // PlayerProgress,
+    ]),
+  ],
+  controllers: [
+    QuizQuestionsSaController,
+    // PairGamesController
+  ],
+  providers: [
+    ...useCases,
+    QuizQuestionsService,
+    QuizQuestionRepository,
+    QuizQuestionQueryRepository,
+    QuizQuestionQueryExternalRepository,
+    // PairGamesService,
+    // PairGamesQueryService,
+    // PairGamesRepository,
+    // PairGamesQueryRepository,
+    // PlayerAnswerService,
+    // PlayerAnswerRepository,
+    // PlayerAnswerQueryRepository,
+    // PlayerProgressRepository,
+    // PlayerProgressQueryRepository,
+  ],
+})
+export class GamesPlatformModule {}
---
+++ src/modules/games-platform/questions/application/usecases/create-question.usecase.ts
@@ -0,0 +1,21 @@
+import { CreateQuizQuestionDto } from '../../dto/create-quiz-question.dto';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { QuizQuestionRepository } from '../../repositories/quiz-question,repository';
+import { QuizQuestion } from '../../entities/quiz-question.entity';
+
+export class CreateQuestionCommand {
+  constructor(public readonly dto: CreateQuizQuestionDto) {}
+}
+
+@CommandHandler(CreateQuestionCommand)
+export class CreateQuestionUseCase implements ICommandHandler<CreateQuestionCommand> {
+  constructor(
+    private readonly quizQuestionRepository: QuizQuestionRepository,
+  ) {}
+
+  async execute({ dto }: CreateQuestionCommand): Promise<string> {
+    const question: QuizQuestion = QuizQuestion.createInstance(dto);
+
+    return this.quizQuestionRepository.saveQuizQuestion(question);
+  }
+}
