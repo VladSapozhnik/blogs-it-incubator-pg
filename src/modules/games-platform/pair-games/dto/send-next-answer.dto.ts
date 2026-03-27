4507c78bcdaff4e8453928390aa26ad3e6c5bdc6
VladSapozhnik <vladbars2@gmail.com>
2026-03-27 12:25:32 +0200

    remove game

--- src/modules/games-platform/pair-games/dto/send-next-answer.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, MinLength } from 'class-validator';
-import { Trim } from '../../../../core/decorators/trim.decorator';
-
-export class SendNextAnswerDto {
-  @IsString()
-  @Trim()
-  @MinLength(1)
-  answer: string;
-}
