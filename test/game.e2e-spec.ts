3c10a9992dc6ebf44ab468249dbc648b94ef5647
VladSapozhnik <vladbars2@gmail.com>
2026-04-08 16:41:43 +0300

    add test for game connect

--- test/game.e2e-spec.ts
+++ test/game.e2e-spec.ts
@@ -7,6 +7,7 @@ import { appSetup } from '../src/setup/app.setup';
 import { deleteAllData } from './helpers/delete-all-data';
 import { constantHelper } from './helpers/constant.helper';
 import { errorMessageHelper } from './helpers/error-message.helper';
+import { GameStatusEnum } from '../src/modules/games-platform/pair-games/enums/game-status.enum';
 
 function getGameDate(
   numberOfPlayers: number = 1,
@@ -32,31 +33,32 @@ function getGameDate(
       startGameDate: null,
       finishGameDate: null,
     };
+  } else if (numberOfPlayers === 2) {
+    return {
+      id: expect.any(String) as string,
+      firstPlayerProgress: {
+        answers: expect.any(Array) as [],
+        player: {
+          id: expect.any(String) as string,
+          login: user1Login,
+        },
+        score: expect.any(Number) as number,
+      },
+      secondPlayerProgress: {
+        answers: expect.any(Array) as [],
+        player: {
+          id: expect.any(String) as string,
+          login: user2Login,
+        },
+        score: expect.any(Number) as number,
+      },
+      questions: expect.any(Array) as [],
+      status: GameStatusEnum.Active,
+      pairCreatedDate: expect.any(String) as string,
+      startGameDate: expect.any(String) as string,
+      finishGameDate: finishGame ? (expect.any(String) as string) : null,
+    };
   }
-  return {
-    id: expect.any(String) as string,
-    firstPlayerProgress: {
-      answers: expect.any(Array) as [],
-      player: {
-        id: expect.any(String) as string,
-        login: user1Login,
-      },
-      score: expect.any(Number) as number,
-    },
-    secondPlayerProgress: {
-      answers: expect.any(Array) as [],
-      player: {
-        id: expect.any(String) as string,
-        login: user2Login,
-      },
-      score: expect.any(Number) as number,
-    },
-    questions: expect.any(Array) as [],
-    status: 'PendingSecondPlayer',
-    pairCreatedDate: expect.any(String) as string,
-    startGameDate: expect.any(String) as string,
-    finishGameDate: finishGame ? (expect.any(String) as string) : null,
-  };
 }
 
 describe('AuthController (e2e)', () => {
@@ -154,77 +156,41 @@ describe('AuthController (e2e)', () => {
   });
 
   it('pair-game-quiz/pairs/connection (POST) should return', async () => {
+    await request(app.getHttpServer())
+      .post('/pair-game-quiz/pairs/connection')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.OK);
+
     const response = await request(app.getHttpServer())
       .post('/pair-game-quiz/pairs/connection')
       .set('Authorization', `Bearer ${tokenUser2}`)
       .expect(HttpStatus.OK);
 
-    // expect(response.body).toEqual(
-    //   getGameDate(
-    //     2,
-    //     constantHelper.users[0].login,
-    //     constantHelper.users[1].login,
-    //   ),
-    // );
+    expect(response.body).toEqual(
+      getGameDate(
+        2,
+        constantHelper.users[0].login,
+        constantHelper.users[1].login,
+      ),
+    );
   });
 
-  // it('/auth/login (POST) should return access token for valid credentials', async () => {
-  //   const response = await request(app.getHttpServer())
-  //     .post('/auth/login')
-  //     .send({
-  //       loginOrEmail: constantHelper.users[0].email,
-  //       password: constantHelper.users[0].password,
-  //     })
-  //     .expect(HttpStatus.OK);
-  //
-  //   expect(response.body).toEqual({
-  //     accessToken: expect.any(String) as string,
-  //   });
-  // });
-  //
-  // it('/auth/login (POST) should return 401 for invalid password', async () => {
-  //   const response = await request(app.getHttpServer())
-  //     .post('/auth/login')
-  //     .send({
-  //       loginOrEmail: constantHelper.users[0].email,
-  //       password: constantHelper.users[1].password,
-  //     })
-  //     .expect(HttpStatus.UNAUTHORIZED);
-  //
-  //   expect(response.body).toEqual(errorMessageHelper());
-  // });
-  //
-  // it('/auth/login (POST) should return 400 for empty login and password', async () => {
-  //   const response = await request(app.getHttpServer())
-  //     .post('/auth/login')
-  //     .send({
-  //       loginOrEmail: '',
-  //       password: '',
-  //     })
-  //     .expect(HttpStatus.BAD_REQUEST);
-  //
-  //   expect(response.body).toEqual(errorMessageHelper(2));
-  // });
-  //
-  // it('/auth/profile (POST) should return user profile for valid token', async () => {
-  //   const response = await request(app.getHttpServer())
-  //     .get('/auth/me')
-  //     .set('Authorization', `Bearer ${token}`)
-  //     .expect(HttpStatus.OK);
-  //
-  //   expect(response.body).toEqual({
-  //     email: expect.any(String) as string,
-  //     login: expect.any(String) as string,
-  //     userId: expect.any(String) as string,
-  //   });
-  // });
-  //
-  // it('/auth/profile (POST) should return 401 for invalid token', async () => {
-  //   const response = await request(app.getHttpServer())
-  //     .get('/auth/me')
-  //     .set('Authorization', `Bearer ${constantHelper.invalidToken}`)
-  //     .expect(HttpStatus.UNAUTHORIZED);
-  //
-  //   expect(response.body).toEqual(errorMessageHelper());
-  // });
+  it('pair-game-quiz/pairs/connection (POST) should return', async () => {
+    await request(app.getHttpServer())
+      .post('/pair-game-quiz/pairs/connection')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.OK);
+
+    await request(app.getHttpServer())
+      .post('/pair-game-quiz/pairs/connection')
+      .set('Authorization', `Bearer ${tokenUser2}`)
+      .expect(HttpStatus.OK);
+
+    const response = await request(app.getHttpServer())
+      .post('/pair-game-quiz/pairs/connection')
+      .set('Authorization', `Bearer ${tokenUser2}`)
+      .expect(HttpStatus.FORBIDDEN);
+
+    expect(response.body).toEqual(errorMessageHelper());
+  });
 });
